57eb389e5f3f6c2a47200a70a6c74c9334cb30c8
Dmitriy <chermenevd@yandex.ru>
2019-01-11 15:10:47 +0300

    Switch to precomp DTB`s

--- projects/Amlogic/packages/device-trees-amlogic/package.mk
+++ projects/Amlogic/packages/device-trees-amlogic/package.mk
@@ -1,74 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
+# Copyright (C) 2019-presents PoPsa85
 
 PKG_NAME="device-trees-amlogic"
-PKG_VERSION="146ec3fef5c08784e1da090366caa67a99017e98"
-PKG_SHA256="728fc67d6366dc5adac2affee8d627dfa0881faca29c756424b7a89f480fdc4d"
+PKG_VERSION="b76caa713b0f0854accedcb2277f975120f75c1b"
+PKG_SHA256="946786db0d36bcdf3c0e3deaa479c9b7cbbaf78b19039126d40cd36b87f076eb"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/CoreELEC/device-trees-amlogic"
-PKG_URL="https://github.com/CoreELEC/device-trees-amlogic/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Device trees for Amlogic devices." 
+PKG_SITE="https://github.com/popsa851/s905-trees"
+PKG_URL="https://github.com/popsa851/s905-trees/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Device trees for Amlogic S905 devices." 
 PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
 
-make_target() {
-  # Enter kernel directory
-  pushd $BUILD/linux-$(kernel_version) > /dev/null
-
-  # Device trees already present in kernel tree we want to include
-  EXTRA_TREES=( \
-                gxbb_p201 gxbb_p200_1G_wetek_hub gxbb_p200_2G_wetek_play_2 \
-                gxl_p212_1g gxl_p212_2g gxl_p281_1g gxl_p212_1g_lepotato gxl_p212_2g_lepotato \
-                gxm_q200_2g gxm_q201_1g gxm_q201_2g gxl_p231_1g_m8s_dvb \
-	      )
-
-  # Add trees to the list
-  for f in ${EXTRA_TREES[@]}; do
-    DTB_LIST="$DTB_LIST $f.dtb"
-  done
-
-  # Copy all device trees to kernel source folder and create a list
-  cp -f $PKG_BUILD/*.dts* arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/
-  for f in $PKG_BUILD/*.dts; do
-    DTB_NAME="$(basename $f .dts).dtb"
-    DTB_LIST="$DTB_LIST $DTB_NAME"
-  done
-
-  # Filter device tree list depending on project
-  case "$DEVICE" in
-    S905)
-      for f in ${DTB_LIST[@]}; do
-        [[ "$f" == gxbb* ]] || [[ "$f" == gxl* ]] && DTB_LIST_FILTERED="$DTB_LIST_FILTERED $f"
-      done
-      ;;
-    S912)
-      for f in ${DTB_LIST[@]}; do
-        [[ "$f" == gxm* ]] && DTB_LIST_FILTERED="$DTB_LIST_FILTERED $f"
-      done
-      ;;
-    *)
-      for f in ${DTB_LIST[@]}; do
-        if listcontains "$KERNEL_UBOOT_EXTRA_TARGET" "$f"; then
-          DTB_LIST_FILTERED="$DTB_LIST_FILTERED $f"
-        fi
-      done
-      ;;
-  esac
-
-  # Compile device trees
-  kernel_make $DTB_LIST_FILTERED
-  cp arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/*.dtb $PKG_BUILD
-
-  popd > /dev/null
-}
-
 makeinstall_target() {
-  case "$DEVICE" in
-    S905|S912)
       mkdir -p $INSTALL/usr/share/bootloader/device_trees
       cp -a $PKG_BUILD/*.dtb $INSTALL/usr/share/bootloader/device_trees
-    ;;
-  esac
 }
